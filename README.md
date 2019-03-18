72df227d9590117c3389495718b2b0bea0b1fc69
Giandomenico Olini <giando.olini@gogodigital.it>
2019-03-18 13:38:07 +0100

    Adding migrations

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Yii2 Dictionary
 Yii2 Dictionary to create, manage, and delete Multilanguage Dictionary in a Yii2 site.
+
+Installation
+-----------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+$ php composer.phar require cinghie/yii2-dictionary "*"
+```
+
+or add
+
+```
+"cinghie/yii2-dictionary": "*"
+```
+
+Configuration
+-----------------
+
+### Update yii2 dictionary database schema
+
+Make sure that you have properly configured `db` application component.  
+Run the following command:
+```
+$ php yii migrate/up --migrationPath=@vendor/cinghie/yii2-dictionary/migrations
+```
---
+++ migrations/m170636_185700_create_dictionary_tables.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * @copyright Copyright &copy; Gogodigital Srls
+ * @company Gogodigital Srls - Wide ICT Solutions
+ * @website http://www.gogodigital.it
+ * @github https://github.com/cinghie/yii2-dictionary
+ * @license GNU GENERAL PUBLIC LICENSE VERSION 3
+ * @package yii2-dictionary
+ * @version 0.1.0
+ */
+
+use cinghie\traits\migrations\Migration;
+
+class m170636_185700_create_dictionary_tables extends Migration
+{
+	/**
+	 * @inheritdoc
+	 */
+	public function safeUp()
+	{
+		$this->createTable('{{%dictionary_keys}}', [
+			'id' => $this->primaryKey(),
+			'key' => $this->string(255)->notNull(),
+		], $this->tableOptions);
+
+		$this->createTable('{{%dictionary_values}}', [
+			'id' => $this->primaryKey(),
+			'key_id' => $this->integer(11),
+			'lang' => $this->string(3)->notNull(),
+			'lang_tag' => $this->string(5)->notNull(),
+		], $this->tableOptions);
+
+		// Add Index and Foreign Key key_id
+		$this->createIndex(
+			'index_dictionary_values_key_id',
+			'{{%dictionary_values}}',
+			'key_id'
+		);
+
+		$this->addForeignKey('fk_dictionary_values_key_id',
+			'{{%dictionary_values}}', 'key_id',
+			'{{%dictionary_keys}}', 'id',
+			'SET NULL', 'CASCADE'
+		);
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function safeDown()
+	{
+		$this->dropTable('{{%dictionary_values}}');
+		$this->dropTable('{{%dictionary_keys}}');
+	}
+}
